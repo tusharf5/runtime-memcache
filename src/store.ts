39bd4db9d9050319f86f6a1a737c647ac94a7ca2
Tushar Sharma <ts17995@gmail.com>
2020-01-18 03:09:43 +0530

    fix: makes config as optional

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,11 @@
 import createHashStore, { UserConfig, CreateHashStoreResult } from './hash-store';
 
-function createStore<K = any>(userConfig: UserConfig): CreateHashStoreResult<K> {
-  const store = createHashStore<K>(userConfig);
+function createStore<K = any>(userConfig?: UserConfig): CreateHashStoreResult<K> {
+  let config = {};
+  if (typeof userConfig === 'object') {
+    config = userConfig;
+  }
+  const store = createHashStore<K>(config);
   return store;
 }
 
